17482aab0ee4969b048b14cbf66e5ec2eb0b2b02
Vishal Pal <vk3187286@gmail.com>
2025-07-31 18:21:38 +0530

    Update index.js

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -14,7 +14,7 @@ const studyRouter = require('./routes/studyPlan');
 
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://leetcode-frontend-bps2.onrender.com/',
     credentials: true
 }));
 

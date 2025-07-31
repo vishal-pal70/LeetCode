a3c9ea9b992283f2faee54f87fcda410b8d71849
Vishal Pal <vk3187286@gmail.com>
2025-07-31 17:57:51 +0530

    Update axiosClient.js

--- Frontend/src/utils/axiosClient.js
+++ Frontend/src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://nexuscode-backend.onrender.com',
     withCredentials: true,
     headers:{
         'content-type': 'application/json'
